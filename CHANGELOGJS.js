7f840e98b8b01fcb17ccdaeb9bfade2394f63197
Rhino Studios <m.hardy1993@aol.co.uk>
2017-04-23 11:24:48 +0100

    FIX TYPOS
    Fixed typos and bad syntax in conditions!

---
+++ CHANGELOGJS.js
@@ -0,0 +1,19 @@
+CHANGELOG
+==================================
+29/03/17 Fixing Phone Number
+	Test 11||12 length
+	Use debug OK? Return undefined_
+	Convert toString use new vars? YES
+			Stored value in new p 
+			pN = p.toString OK
+	GLOBALS - Reset count each time 
+		Checks if has been done OK
+=================================
+ Still Valid? WHY 
+Form valid even if phone too short!
+Changed length==11||12 to is 11&&12 YES
+
+	
+	
+	
+	
